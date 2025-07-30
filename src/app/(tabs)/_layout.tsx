134457a3fc673b7c7827ae40a29b250cb5e382f0
GKyomen <kenkakyomen@hotmail.com>
2025-07-29 22:37:56 -0300

    ✨ Feat: tab bar (+ icons & page names)

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -1,12 +1,71 @@
 import { Tabs } from "expo-router";
+import { HomeIcon, MissionsIcon, GoalsIcon, ExperiencesIcon, MenuIcon } from "@/components/TabIcons";
 
 export default function TabLayout() {
   return (
     <Tabs
       backBehavior="history"
-      screenOptions={{ headerShown: false }}
+      screenOptions={{
+        headerShown: false,
+        tabBarStyle: {
+          elevation: 0,
+          shadowOpacity: 0,
+          borderTopWidth: 0,
+          backgroundColor: '#F2EFE0',
+        },
+        tabBarActiveTintColor: '#1E2543',
+        tabBarInactiveTintColor: '#9D9EA1',
+        tabBarLabelStyle: {
+          fontFamily: 'Poppins_600SemiBold',
+          fontSize: 10,
+        },
+      }}
     >
-      <Tabs.Screen name="index" options={{ title: "Home" }} />
+      <Tabs.Screen 
+        name="index" 
+        options={{ 
+          title: "Início",
+          tabBarIcon: ({ color }) => (
+            <HomeIcon color={color} size={18} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="missions" 
+        options={{ 
+          title: "Missões",
+          tabBarIcon: ({ color }) => (
+            <MissionsIcon color={color} size={20} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="goals" 
+        options={{ 
+          title: "Metas",
+          tabBarIcon: ({ color }) => (
+            <GoalsIcon color={color} size={26} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="experiences" 
+        options={{ 
+          title: "Experiências",
+          tabBarIcon: ({ color }) => (
+            <ExperiencesIcon color={color} size={22} />
+          ),
+        }} 
+      />
+      <Tabs.Screen 
+        name="menu" 
+        options={{ 
+          title: "Menu",
+          tabBarIcon: ({ color }) => (
+            <MenuIcon color={color} size={19} />
+          ),
+        }} 
+      />
     </Tabs>
   );
 }
