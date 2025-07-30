009905e60c752ad8ebe63f3117fa786b4f2c5ba4
GKyomen <kenkakyomen@hotmail.com>
2025-07-29 22:37:21 -0300

    ✨ Feat: dummy pages for bottom nav bar

---
+++ src/app/(tabs)/missions.tsx
@@ -0,0 +1,31 @@
+import { View, Text, StyleSheet } from 'react-native';
+import { colors } from '@/styles/colors';
+
+export default function Missoes() {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.title}>Missões</Text>
+      <Text style={styles.description}>Página não desenvolvida para o teste</Text>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  title: {
+    fontSize: 24,
+    fontFamily: 'Poppins_600SemiBold',
+    textAlign: 'center',
+    color: colors.primary,
+  },
+  description: {
+    fontSize: 16,
+    fontFamily: 'Poppins_400Regular',
+    textAlign: 'center',
+    color: colors.gray,
+  }
+});
